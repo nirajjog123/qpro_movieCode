39575f4cfdb89c47f483b066b09dbf50a423c96d
Niraj <niraj@www.questionpro.com>
2019-06-06 12:44:54 +0530

    basic layout of qpro_app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router , Route, Link} from 'react-router-dom';
+import MvList from './mList/mList';
+import MvAdd from './mAdd/mAdd';
+import MvInfo from './mInfo/mInfo';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Route exact path='/list' component={MvList}></Route>
+      <Route  path='/add' component={MvAdd}></Route>
+      <Route  path='/info' component={MvInfo}></Route>
+      
+      </Router>    
     </div>
   );
 }
---
+++ src/mList/mList.js
@@ -0,0 +1,13 @@
+import React , {Component} from 'react';
+
+
+class mList extends Component{
+
+constructor(){
+    super();
+}
+
+
+
+
+}
